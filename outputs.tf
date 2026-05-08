1675df06e6360dee9bb5f82e08fcb7cbbaf389aa
Geoffrey Moraes <social@eddietorial.net>
2026-05-08 18:31:29 +0530

    Rename outputs for clarity: ip -> instance_public_ip, id -> instance_id

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,10 @@
-output "id" {
-  value = aws_instance.wordpress.arn
+output "instance_id" {
+  description = "ARN of the WordPress EC2 instance"
+  value       = aws_instance.wordpress.arn
 }
 
-output "ip" {
-  description = "Instance ip, use http://<ip> to connect to wordpress and ssh ubuntuo@<ip> for ssh"
-  value = aws_instance.wordpress.public_ip 
+output "instance_public_ip" {
+  description = "Public IP of the WordPress instance. Visit http://<ip> in a browser or connect via ssh ubuntu@<ip>"
+  value       = aws_instance.wordpress.public_ip
 }
+
